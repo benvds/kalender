930587caeccb0b211507413a3861e8f25e6c8a59
benvds <ben@pomodoro.nl>
2015-04-04 11:52:08 +0200

    add tests for correct number of days

--- kalender.js
+++ kalender.js
@@ -1,9 +1,31 @@
 'use strict';
 
-// var _ = require('lodash');
+var _ = require('lodash');
+
+var DAYS_PER_MONTH = {
+    '28': [2],
+    '30': [4, 6, 9, 11],
+    '31': [1, 3, 5, 7, 8, 10, 12]
+};
 
 function getMonth(year, month) {
-    return [year, month];
+    var dates = []
+
+    for (var date = 1, amount = amountOfDays(year, month);
+         date <= amount;
+         date++)
+    {
+        dates.push(date);
+    }
+
+    return dates;
+}
+
+function amountOfDays(year, month) {
+    return Number(_.reduce(DAYS_PER_MONTH,
+                           function(result, months, daysForMonth) {
+        return _.contains(months, month) ? daysForMonth : result;
+    }, 0));
 }
 
 var kalender = {
--- test/test.js
+++ test/test.js
@@ -1,10 +1,39 @@
 var k = require('../kalender'),
     assert = require('assert');
 
-describe('kalender', function(){
-    describe('getMonth(year, month)', function() {
-        it('returns [year, month]', function(){
-            assert.deepEqual([2015, 1], k.getMonth(2015, 1));
+describe('kalender.getMonth(year, month)', function(){
+    describe('returns the correct number of days', function() {
+        var year = 2015;
+
+        it('for months with 31 days', function() {
+            var MONTHS_WITH_31_DAYS = [1, 3, 5, 7, 8, 10, 12];
+
+            MONTHS_WITH_31_DAYS.forEach(function(month) {
+                assert.equal(31, k.getMonth(year, month).length);
+            });
         });
+
+        it('for months with 30 days', function() {
+            var MONTHS_WITH_30_DAYS = [4, 6, 9, 11];
+
+            MONTHS_WITH_30_DAYS.forEach(function(month) {
+                assert.equal(30, k.getMonth(year, month).length);
+            });
+        });
+
+        it('for months with 28 days', function() {
+            var MONTHS_WITH_28_DAYS = [2];
+
+            MONTHS_WITH_28_DAYS.forEach(function(month) {
+                assert.equal(28, k.getMonth(year, month).length);
+            });
+        });
+
+        /*
+         * https://en.wikipedia.org/wiki/Leap_year#Algorithm
+         * http://stackoverflow.com/questions/725098/leap-year-calculation
+         * http://www.dispersiondesign.com/articles/time/determining_leap_years
+         */
+        it('for months with 29 days');
     });
 });
