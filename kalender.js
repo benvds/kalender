702d25eda3e33c8162d8085fa765f39cffeff12d
benvds <ben@pomodoro.nl>
2015-04-05 23:51:00 +0200

    add leap years

--- kalender.js
+++ kalender.js
@@ -3,10 +3,11 @@
 var _ = require('lodash');
 
 var DAYS_PER_MONTH = {
-    '28': [2],
-    '30': [4, 6, 9, 11],
-    '31': [1, 3, 5, 7, 8, 10, 12]
-};
+        '28': [2],
+        '30': [4, 6, 9, 11],
+        '31': [1, 3, 5, 7, 8, 10, 12]
+    },
+    MONTH_WITH_ADDITIONAL_DAY_ON_LEAP_YEAR = 2;
 
 function getMonth(year, month) {
     var dates = []
@@ -22,11 +23,23 @@ function getMonth(year, month) {
 }
 
 function amountOfDays(year, month) {
-    return Number(_.reduce(DAYS_PER_MONTH,
+    // TODO refactor into a expanded "find" function to increase readability
+    var amount = Number(_.reduce(DAYS_PER_MONTH,
                            function(result, months, daysForMonth) {
         return _.contains(months, month) ? daysForMonth : result;
     }, 0));
+
+    if (month === MONTH_WITH_ADDITIONAL_DAY_ON_LEAP_YEAR && isLeapYear(year)) {
+        amount++;
+    }
+
+    return amount;
 }
+
+function isLeapYear(year) {
+    return ((year % 4 === 0) && (year % 100 != 0)) || (year % 400 === 0);
+}
+
 
 var kalender = {
     getMonth: getMonth
--- test/test.js
+++ test/test.js
@@ -3,37 +3,40 @@ var k = require('../kalender'),
 
 describe('kalender.getMonth(year, month)', function(){
     describe('returns the correct number of days', function() {
-        var year = 2015;
+        var LEAP_YEARS = [1996, 2000, 2004],
+            NON_LEAP_YEARS = [1997, 1998, 1999, 2100],
+            MONTHS_WITH_31_DAYS = [1, 3, 5, 7, 8, 10, 12],
+            MONTHS_WITH_30_DAYS = [4, 6, 9, 11],
+            MONTHS_WITH_29_OR_28_DAYS = [2],
+            year = 2015;
 
         it('for months with 31 days', function() {
-            var MONTHS_WITH_31_DAYS = [1, 3, 5, 7, 8, 10, 12];
-
             MONTHS_WITH_31_DAYS.forEach(function(month) {
                 assert.equal(31, k.getMonth(year, month).length);
             });
         });
 
         it('for months with 30 days', function() {
-            var MONTHS_WITH_30_DAYS = [4, 6, 9, 11];
-
             MONTHS_WITH_30_DAYS.forEach(function(month) {
                 assert.equal(30, k.getMonth(year, month).length);
             });
         });
 
-        it('for months with 28 days', function() {
-            var MONTHS_WITH_28_DAYS = [2];
-
-            MONTHS_WITH_28_DAYS.forEach(function(month) {
-                assert.equal(28, k.getMonth(year, month).length);
+        it('for months with 29 days', function () {
+            LEAP_YEARS.forEach(function(year) {
+                MONTHS_WITH_29_OR_28_DAYS.forEach(function(month) {
+                    assert.equal(29, k.getMonth(year, month).length);
+                });
             });
         });
 
-        /*
-         * https://en.wikipedia.org/wiki/Leap_year#Algorithm
-         * http://stackoverflow.com/questions/725098/leap-year-calculation
-         * http://www.dispersiondesign.com/articles/time/determining_leap_years
-         */
-        it('for months with 29 days');
+        it('for months with 28 days', function() {
+            NON_LEAP_YEARS.forEach(function(year) {
+                MONTHS_WITH_29_OR_28_DAYS.forEach(function(month) {
+                    assert.equal(28, k.getMonth(year, month).length);
+                });
+            });
+        });
+
     });
 });
