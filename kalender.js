269b0be3148bf0578c31e65c62f198445981b568
benvds <ben@pomodoro.nl>
2015-04-06 19:27:52 +0200

    add getCalendar

--- kalender.js
+++ kalender.js
@@ -2,40 +2,107 @@
 
 var _ = require('lodash');
 
-var DAYS_PER_MONTH = {
+var DAYS_PER_WEEK = 7,
+    MONTHS_PER_YEAR = 12,
+    DAYS_PER_MONTH = {
         '28': [2],
         '30': [4, 6, 9, 11],
         '31': [1, 3, 5, 7, 8, 10, 12]
     },
     MONTH_WITH_ADDITIONAL_DAY_ON_LEAP_YEAR = 2;
 
+function getCalendar(month, options) {
+    var beforeDays = getDays(previousMonth(month));
+    var mainDays = getDays(month);
+    var afterDays = getDays(nextMonth(month));
 
-function getMonth(year, month) {
-    var dates = []
+    var openWeekDaysBefore = dayOfWeek(mainDays[0]);
+    var openWeekDaysAfter = (DAYS_PER_WEEK - 1) - dayOfWeek(_.last(mainDays));
 
-    for (var date = 1, amount = amountOfDays(year, month);
-         date <= amount;
-         date++)
-    {
-        dates.push(date);
+    var daysBefore = [],
+        daysAfter = [];
+
+    if (openWeekDaysBefore) {
+        daysBefore = beforeDays.slice(-1 * openWeekDaysBefore);
     }
 
-    return dates;
+    if (openWeekDaysAfter) {
+        daysAfter = afterDays.slice(0, openWeekDaysAfter);
+    }
+
+    return []
+        .concat(daysBefore)
+        .concat(mainDays)
+        .concat(daysAfter);
 }
 
-function amountOfDays(year, month) {
-    var daysForMonth = Number(_.findKey(DAYS_PER_MONTH, function(months) {
-        return _.contains(months, month);
+function dayOfWeek(day) {
+    var date = new Date(Date.UTC(day.year, day.month - 1, day.day));
+
+    return date.getDay();
+}
+
+// TODO: confusing, getDays returns an array of days, month is an object with month & year
+function getDays(month) {
+    var days = []
+
+    for (var day = 1, amount = amountOfDays(month);
+         day <= amount;
+         day++)
+    {
+        days.push({
+            year: month.year,
+            month: month.month,
+            day: day
+        });
+    }
+
+    return days;
+}
+
+function previousMonth(month) {
+    if (month.month === 1) {
+        return {
+            year: (month.year - 1),
+            month: MONTHS_PER_YEAR
+        };
+    } else {
+        return {
+            year: month.year,
+            month: (month.month - 1)
+        };
+    }
+}
+
+function nextMonth(month) {
+    if (month.month === MONTHS_PER_YEAR) {
+        return {
+            year: (month.year + 1),
+            month: 1
+        };
+    } else {
+        return {
+            year: month.year,
+            month: (month.month + 1)
+        };
+    }
+}
+
+function amountOfDays(month) {
+    var amount = Number(_.findKey(DAYS_PER_MONTH, function(months) {
+        return _.contains(months, month.month);
     }));
 
-    return addLeapDay(daysForMonth, year, month);
+    return addLeapDay(amount, month);
 }
 
-function addLeapDay(daysForMonth, year, month) {
-    if (month === MONTH_WITH_ADDITIONAL_DAY_ON_LEAP_YEAR && isLeapYear(year)) {
-        return daysForMonth + 1;
+function addLeapDay(amountOfDays, month) {
+    if (month.month === MONTH_WITH_ADDITIONAL_DAY_ON_LEAP_YEAR &&
+        isLeapYear(month.year))
+    {
+        return amountOfDays + 1;
     } else {
-        return daysForMonth;
+        return amountOfDays;
     }
 }
 
@@ -44,7 +111,8 @@ function isLeapYear(year) {
 }
 
 var kalender = {
-    getMonth: getMonth
+    getDays: getDays,
+    getCalendar: getCalendar
 };
 
 module.exports = kalender;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var k = require('../kalender'),
     assert = require('assert');
 
-describe('kalender.getMonth(year, month)', function(){
+describe('kalender.getDays(year, month)', function(){
     describe('returns the correct number of days', function() {
         var LEAP_YEARS = [1996, 2000, 2004],
             NON_LEAP_YEARS = [1997, 1998, 1999, 2100],
@@ -12,20 +12,20 @@ describe('kalender.getMonth(year, month)', function(){
 
         it('for months with 31 days', function() {
             MONTHS_WITH_31_DAYS.forEach(function(month) {
-                assert.equal(31, k.getMonth(year, month).length);
+                assert.equal(31, k.getDays({ year: year, month: month }).length);
             });
         });
 
         it('for months with 30 days', function() {
             MONTHS_WITH_30_DAYS.forEach(function(month) {
-                assert.equal(30, k.getMonth(year, month).length);
+                assert.equal(30, k.getDays({ year: year, month: month }).length);
             });
         });
 
         it('for months with 29 days', function () {
             LEAP_YEARS.forEach(function(year) {
                 MONTHS_WITH_29_OR_28_DAYS.forEach(function(month) {
-                    assert.equal(29, k.getMonth(year, month).length);
+                    assert.equal(29, k.getDays({ year: year, month: month }).length);
                 });
             });
         });
@@ -33,10 +33,53 @@ describe('kalender.getMonth(year, month)', function(){
         it('for months with 28 days', function() {
             NON_LEAP_YEARS.forEach(function(year) {
                 MONTHS_WITH_29_OR_28_DAYS.forEach(function(month) {
-                    assert.equal(28, k.getMonth(year, month).length);
+                    assert.equal(28, k.getDays({ year: year, month: month }).length);
                 });
             });
         });
+    });
 
+    describe('sibling months', function() {
+        it('discards previous month when current month starts on weekStart',
+           function()
+        {
+            // default weekStart = 0 = sunday
+            var MONTH_WITH_FIRST_DAY_ON_WEEKSTART = {
+                    year: 2015,
+                    month: 3
+                },
+                options = {
+                    includeAdjacentMonths: true
+                };
+            assert.deepEqual(
+                {
+                    year: 2015,
+                    month: 3,
+                    day: 1
+                },
+                k.getCalendar(MONTH_WITH_FIRST_DAY_ON_WEEKSTART, options)[0]);
+        });
+
+        it('discards next month when current month ends on weeks end',
+           function()
+        {
+            // default weekStart = 0 = sunday
+            var MONTH_WITH_LAST_DAY_ON_WEEKS_END = {
+                    year: 2015,
+                    month: 2
+                },
+                options = {
+                    includeAdjacentMonths: true
+                };
+            var result = k.getCalendar(MONTH_WITH_LAST_DAY_ON_WEEKS_END,
+                                       options);
+            assert.deepEqual(
+                {
+                    year: 2015,
+                    month: 2,
+                    day: 28
+                },
+                result[result.length - 1]);
+        });
     });
 });
