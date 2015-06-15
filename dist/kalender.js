a612a0a640bd057ad6fd61c752b37d0acd469879
benvds <ben@pomodoro.nl>
2015-06-15 09:41:42 +0200

    build

--- dist/kalender.js
+++ dist/kalender.js
@@ -169,6 +169,12 @@ module.exports = calendar;
 },{"./day":3,"./month":4}],3:[function(require,module,exports){
 'use strict';
 
+var DAY_WEIGHTS = {
+    year: 385,
+    month: 32,
+    day: 1
+};
+
 /**
  * Returns day of week for given day. 1 for sunday, 7 for monday.
  *
@@ -185,8 +191,68 @@ function dayOfWeek(day) {
     return date.getDay();
 }
 
+/**
+ * Returns if subject day is before comparison day.
+ *
+ * @argument {Object} subject day
+ * @argument {Object} comparsion day
+ *
+ * @returns {Boolean} true when subject day is before comparison day
+ */
+function isBefore(subject, comparison) {
+    return dayWeight(subject) < dayWeight(comparison);
+}
+
+/**
+ * Returns if subject day is after comparison day.
+ *
+ * @argument {Object} subject day
+ * @argument {Object} comparsion day
+ *
+ * @returns {Boolean} true when subject day is after comparison day
+ */
+function isAfter(subject, comparison) {
+    return dayWeight(subject) > dayWeight(comparison);
+}
+
+/**
+ * Returns weight for a day which can be used in comparisons. Weights are not
+ * relative to each other. Later dates only have higher weights. Using weights
+ * is more than a 100 times faster than creating a date and getting the
+ * primitive value.
+ *
+ * @argument {Object} day
+ * @argument {Number} day.year
+ * @argument {Number} day.month
+ * @argument {Number} day.day
+ *
+ * @returns {Number} dayWeight timestamp for start of day
+ */
+function dayWeight(day) {
+    return (day.day * DAY_WEIGHTS.day) +
+        (day.month * DAY_WEIGHTS.month) +
+        (day.year * DAY_WEIGHTS.year);
+}
+
+/**
+ * Returns if subject day is the same as the comparison day.
+ *
+ * @argument {Object} subject day
+ * @argument {Object} comparsion day
+ *
+ * @returns {Boolean} true when subject day is the same as the comparison day
+ */
+function isEqual(subject, comparison) {
+    return (subject.day === comparison.day) &&
+        (subject.month === comparison.month) &&
+        (subject.year === comparison.year);
+}
+
 module.exports = {
-    dayOfWeek: dayOfWeek
+    dayOfWeek: dayOfWeek,
+    isBefore: isBefore,
+    isAfter: isAfter,
+    isEqual: isEqual
 };
 
 },{}],4:[function(require,module,exports){
@@ -316,12 +382,6 @@ module.exports = {
 },{"./day":3,"./year":6}],5:[function(require,module,exports){
 'use strict';
 
-var DAY_WEIGHTS = {
-    year: 385,
-    month: 32,
-    day: 1
-};
-
 /**
  * Returns a new calendar with the results of calling a provided callback
  * function on every day.
@@ -337,53 +397,8 @@ function mapDays(calendar, callback) {
     });
 }
 
-/**
- * Returns if subject day is before comparison day.
- *
- * @argument {Object} subject day
- * @argument {Object} comparsion day
- *
- * @returns {Boolean} true when subject day is before comparison day
- */
-function isBefore(subject, comparison) {
-    return dayWeight(subject) < dayWeight(comparison);
-}
-
-/**
- * Returns if subject day is after comparison day.
- *
- * @argument {Object} subject day
- * @argument {Object} comparsion day
- *
- * @returns {Boolean} true when subject day is after comparison day
- */
-function isAfter(subject, comparison) {
-    return dayWeight(subject) > dayWeight(comparison);
-}
-
-/**
- * Returns weight for a day which can be used in comparisons. Weights are not
- * relative to each other. Later dates only have higher weights. Using weights
- * is more than a 100 times faster than creating a date and getting the
- * primitive value.
- *
- * @argument {Object} day
- * @argument {Number} day.year
- * @argument {Number} day.month
- * @argument {Number} day.day
- *
- * @returns {Number} dayWeight timestamp for start of day
- */
-function dayWeight(day) {
-    return (day.day * DAY_WEIGHTS.day) +
-        (day.month * DAY_WEIGHTS.month) +
-        (day.year * DAY_WEIGHTS.year);
-}
-
 module.exports = {
-    mapDays: mapDays,
-    isBefore: isBefore,
-    isAfter: isAfter
+    mapDays: mapDays
 };
 
 },{}],6:[function(require,module,exports){
