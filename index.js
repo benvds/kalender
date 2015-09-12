1f1e7170f4633f20daae1e9c796d856e807c597a
benvds <ben@pomodoro.nl>
2015-09-12 19:22:39 +0200

    remove year

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import Year from './lib/year';
 import Month from './lib/month';
 import Day from './lib/day';
 import * as util from './lib/util';
 import Calendar from './lib/calendar';
 
 export {
-    Year,
     Month,
     Day,
     Calendar,
